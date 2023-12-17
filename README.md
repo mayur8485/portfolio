f67a3ae8506d478c4968dab27a41b9e69abff2bb
Mayuresh <mayuresh.takawane@dista.ai>
2023-12-17 23:28:50 +0530

    First Commit with basic code and styles for intro

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # portfolio
 My Portfolio
+
+This has description about the technology i know and learned. It includes projects i did.
---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    var typed = new Typed(".subtitle-content",
+        {
+            strings: ['Software Developer.', 'Frontend Developer.', ' Data Analytics.'],
+            typeSpeed: 70,
+            backSpeed: 40,
+            loop: true
+        }
+    );
+
+})
