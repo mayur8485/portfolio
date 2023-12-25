16334bb26ee96911dca82bada80eccf4a9671270
Mayuresh <mayuresh.takawane@dista.ai>
2023-12-25 13:53:22 +0530

    adding contact section basic footer section

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+function sendMessage() {
+    // form data 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed(".subtitle-content",
         {
-            strings: ['Software Developer.', 'Frontend Developer.', ' Data Analytics.'],
+            strings: ['Software Developer.', 'Frontend Developer (Angular).', ' Data Analytics.'],
             typeSpeed: 70,
             backSpeed: 40,
             loop: true
