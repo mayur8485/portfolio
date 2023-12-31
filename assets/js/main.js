3035914f942446b3eca5edc1faadb012a5e3597b
Mayuresh <mayuresh.takawane@dista.ai>
2023-12-31 12:52:38 +0530

    Nav tabs active based on sections on viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,36 @@ function sendMessage() {
     const message = document.getElementById('message').value;
 }
 
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function checkActiveSection() {
+    const sections = document.getElementsByTagName("section");
+    const nav_menu = document.querySelector('ul.nav');
+    let isLastActive = nav_menu.querySelector("a[href$='#" + 'intro' + "']").parentNode;
+
+    window.addEventListener("scroll", () => {
+        for (let i = 0; i < sections.length; i++) {
+            if (isInViewport(sections[i])) {
+                const active_tab = nav_menu.querySelector("a[href$='#" + sections[i].id + "']").parentNode;
+                if (isLastActive) {
+                    isLastActive.classList.remove('active');
+                    console.log(isLastActive)
+                    isLastActive = active_tab;
+                }
+                active_tab.classList.add('active');
+            }
+        }
+    })
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     var typed = new Typed(".subtitle-content",
@@ -17,16 +47,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     );
 
-    var navItems = document.querySelectorAll(".nav-item");
-    let lastActive = document.querySelector('li.active');
-    for (var i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener("click", function () {
-            console.log(lastActive)
-            this.classList.add("active");
-            lastActive.classList.remove("active");
-            lastActive = this;
-            console.log(this)
-        });
+
+    // check Active NavTab
+    const sections = document.getElementsByTagName("section");
+    const nav_menu = document.querySelector('ul.nav');
+    for (let i = 0; i < sections.length; i++) {
+        if (isInViewport(sections[i])) {
+            const active_tab = nav_menu.querySelector("a[href$='#" + sections[i].id + "']").parentNode;
+            active_tab.classList.add('active');
+        }
     }
 
+    checkActiveSection();
 })
